5fa7146de8b7979d8b83805ce3c4f134ca962335
gabriel pacheco <gabrielpacheco.gpr@gmail.com>
2021-03-19 16:40:33 -0400

    Implementada operacoes assincronas

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -15,110 +15,110 @@ namespace SalesWebMvc.Controllers
     {
         private readonly SellerService _sellerService;
         private readonly DepartmentService _departmentService;
-        
+
         public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _departmentService = departmentService;
             _sellerService = sellerService;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var list = _sellerService.FindAll();
+            var list = await _sellerService.FindAllAsync();
             return View(list);
         }
 
-        public IActionResult Create()
+        public async Task<IActionResult> Create()
         {
-            var departments = _departmentService.FindAll();
+            var departments = await _departmentService.FindAllAsync();
             var viewModel = new SellerFormViewModel { Departments = departments };
             return View(viewModel);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Seller seller)
+        public async Task<IActionResult> Create(Seller seller)
         {
             if (!ModelState.IsValid)
             {
-                var departments = _departmentService.FindAll();
+                var departments = await _departmentService.FindAllAsync();
                 var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
                 return View(seller);
             }
-            _sellerService.Insert(seller);
+            await _sellerService.InsertAsync(seller);
             return RedirectToAction(nameof(Index));
         }
 
-        public IActionResult Delete(int? id)
-        {
-            if(id == null)
-            {
-                return RedirectToAction(nameof(Error), new{ message = "ID nao encontrado"});
-            }
-            var obj = _sellerService.FindById(id.Value);
-            if(obj == null)
-            {
-                return RedirectToAction(nameof(Error), new{ message = "ID nao encontrado"});
-            }
-
-            return View(obj);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Delete(int id, Seller seller)
-        {
-            if (!ModelState.IsValid)
-            {
-                var departments = _departmentService.FindAll();
-                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
-                return View(seller);
-            }
-            _sellerService.Remove(id);
-            return RedirectToAction(nameof(Index));
-        }
-
-        public IActionResult Details(int? id)
+        public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
             {
                 return RedirectToAction(nameof(Error), new { message = "ID nao encontrado" });
             }
-            var obj = _sellerService.FindById(id.Value);
+            var obj = await _sellerService.FindByIdAsync(id.Value);
             if (obj == null)
             {
-                return RedirectToAction(nameof(Error), new{ message = "ID nao encontrado"});
+                return RedirectToAction(nameof(Error), new { message = "ID nao encontrado" });
             }
 
             return View(obj);
         }
 
-        public IActionResult Edit(int? id)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id, Seller seller)
         {
-            if(id == null)
+            await _sellerService.RemoveAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
             {
-                return RedirectToAction(nameof(Error), new{ message = "ID nao encontrado"});
+                return RedirectToAction(nameof(Error), new { message = "ID nao encontrado" });
             }
-            var obj = _sellerService.FindById(id.Value);
-            if(obj == null)
+            var obj = await _sellerService.FindByIdAsync(id.Value);
+            if (obj == null)
             {
-                return RedirectToAction(nameof(Error), new{ message = "ID nao encontrado"});
+                return RedirectToAction(nameof(Error), new { message = "ID nao encontrado" });
             }
 
-            List<Department> departments = _departmentService.FindAll();
-            SellerFormViewModel ViewModel = new SellerFormViewModel { Seller = obj, Departments = departments};
+            return View(obj);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "ID nao encontrado" });
+            }
+            var obj =await _sellerService.FindByIdAsync(id.Value);
+            if (obj == null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "ID nao encontrado" });
+            }
+
+            List<Department> departments =await _departmentService.FindAllAsync();
+            SellerFormViewModel ViewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
             return View(ViewModel);
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(int id, Seller seller)
+        public async Task<IActionResult> Edit(int id, Seller seller)
         {
-            if(id != seller.Id)
+            if (!ModelState.IsValid)
             {
-                return RedirectToAction(nameof(Error), new{ message = "ID nao correspodem"});
+                var departments = await _departmentService.FindAllAsync();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(seller);
+            }
+            if (id != seller.Id)
+            {
+                return RedirectToAction(nameof(Error), new { message = "ID nao correspodem" });
             }
             try
             {
-                _sellerService.Update(seller);
+                await _sellerService.UpdateAsync(seller);
                 return RedirectToAction(nameof(Index));
             }
             catch (ApplicationException e)
