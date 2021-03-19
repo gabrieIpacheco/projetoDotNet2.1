e2526ed24020e89db001bb7fac690299e0066465
gabriel pacheco <gabrielpacheco.gpr@gmail.com>
2021-03-19 16:21:38 -0400

    validacoes implementadas

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -8,17 +8,25 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="{0} é obrigatório")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} deve ter entre {2} e {1} caracteres")]
         [Display(Name = "Nome")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "{0} é obrigatório")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "Email inválido")]
         [Display(Name = "Data de nascimento")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+        [Range(100.0, 5000.0, ErrorMessage ="{0} deve ser entre {1} e {2}")]
+        [Required(ErrorMessage = "{0} é obrigatório")]
         [Display(Name = "Salario base")]
         [DisplayFormat(DataFormatString = "{0:f2}")]
         public double BaseSalary { get; set; }
+        [Required(ErrorMessage = "{0} é obrigatório")]
         [Display(Name = "Departamento")]
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
